dc60c6d55a584bbe8418510e52196925cf1d1a07
Ayesh <ayeshweerasinghe.16@cse.mrt.ac.lk>
2019-03-16 03:48:44 +0530

    Database connection class was added

---
+++ src/dbUtil/dbConnection.java
@@ -0,0 +1,20 @@
+package dbUtil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbConnection {
+
+    private static final String conn = "jdbc:sqlite:Classifer.sqlite";
+
+    public static Connection getConnection() throws SQLException{
+        try{
+            Class.forName("org.sqlite.JDBC");
+            return DriverManager.getConnection(conn);
+        } catch (ClassNotFoundException ex){
+            ex.printStackTrace();
+        }
+        return null;
+    }
+}
